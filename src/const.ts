6a566f97084d39267ce509deab0f749b4a7bf95e
Kobayashi <kobayashi@fukuroaki.anonaddy.com>
2025-04-09 21:51:07 +0700

    Rebase

--- src/const.ts
+++
@@ -1,4 +0,0 @@
-// src/const.ts
-export const ADDRESS = Deno.env.get("ADDRESS") ?? "";
-export const TOKEN = Deno.env.get("TOKEN") ?? "";
-export const WORKER_ADDRESS = Deno.env.get("WORKER_ADDRESS") ?? "";
--- src/handleRequest.ts
+++
@@ -1,8 +0,0 @@
-// src/handleRequest.ts
-import { handleDownload } from "./handleDownload.ts";
-import { handleOptions } from "./handleOptions.ts";
-
-export const handleRequest = async (request: Request): Promise<Response> => {
-  if (request.method === "OPTIONS") return handleOptions(request);
-  return handleDownload(request);
-};
